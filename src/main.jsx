2482fe25d544ffeb891a68eece52c2146ac32a05
apple <apple@appleui-MacBookAir.local>
2026-05-21 13:48:26 +0900

    reset.css 파일 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/theme.css";
 import "./styles/global.css";
-import "./styles/theme.css";
 import App from "./App.jsx";
-import "./styles/theme.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
