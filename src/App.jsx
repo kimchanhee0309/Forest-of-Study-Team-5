b7afec95e759ff07ca3daa85341389cf39211e9b
KIMJIHUN <kimjihun4118@gmail.com>
2026-05-27 18:06:08 +0900

    refactor:공용 작업 컴포넌트 초기 상태로 복원

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-// 1. 기존 GuidePage를 지우고, 방금 만든 HabitPage를 불러옵니다.
-import HabitPage from "./pages/Habitpages/HabitPage";
+import GuidePage from "./components/guide/GuidePage.jsx";
 
 function App() {
-  return (
-    <div>
-      {/* 2. HabitPage 컴포넌트를 사용합니다. */}
-      <HabitPage />
-    </div>
-  );
+  return <GuidePage />;
 }
 
 export default App;
